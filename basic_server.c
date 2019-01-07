3b48f3bdc59b8dd975754aeff9233eb894abf964
Michael Luo <33794837+RealMichael@users.noreply.github.com>
2019-01-07 00:04:20 -0500

    ...

--- basic_server.c
+++ basic_server.c
@@ -7,4 +7,14 @@ int main() {
   int from_client;
 
   from_client = server_handshake( &to_client );
+
+	char se[BUFFER_SIZE];
+		
+	while(read(from_client,se,BUFFER_SIZE)){
+	
+	write(to_client,se,BUFFER_SIZE);
+	
+	}
+return 0;
+
 }
--- client.c
+++ client.c
@@ -3,8 +3,25 @@
 
 int main() {
 
-  int to_server;
-  int from_server;
+	int to_server;
+	int from_server;
 
-  from_server = client_handshake( &to_server );
+	char send[BUFFER_SIZE];
+	char get[BUFFER_SIZE];
+	
+
+
+	from_server = client_handshake( &to_server );
+	
+	while (1){
+		printf("TYPE A MESSAGE!");	
+		fgets(send,BUFFER_SIZE,stdin);
+		write(to_server,send,256);
+		printf("wrote: %s\n",send);
+
+		read(from_server,get,256);
+		printf("got: %s\n",get);
+	}
+
+	return 0;
 }
